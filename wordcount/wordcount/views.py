431fc7763107b730ada4fa82c92467da917bcf4c
unknown <mdebaprasad14@gmail.com>
2019-06-30 01:58:45 +0530

    sort and display words based on count

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
@@ -8,6 +9,13 @@ def count(request):
     ourtext = request.GET['ourtext']
 
     wordlist = ourtext.split()
+    word_dict = {}
 
+    for word in wordlist:
+        if word in word_dict:
+            word_dict[word] += 1    # if word already present then increse count
+        else:
+            word_dict[word] = 1     # if word not present then initialize count to 1
 
-    return render(request, 'count.html', {'ourtext':ourtext, 'count':len(wordlist)})
+    sortedWords = sorted(word_dict.items(), key= operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'ourtext':ourtext, 'count':len(wordlist), 'sortedWords':sortedWords})
